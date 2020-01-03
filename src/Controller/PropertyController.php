af72ec155b06ba4ea3dbd3df2eb351bd32fba5ce
JennyViannay <jenny.viannay75@gmail.com>
2020-01-03 18:00:47 +0100

    add property entity, home's view, list property's view

---
+++ src/Controller/PropertyController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\PropertyRepository;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PropertyController extends AbstractController
+{
+    /**
+     * @var PropertyRepository
+     */
+    private $repository;
+
+    /**
+     * @var ObjectManager
+     */
+    private $em;
+
+    public function __construct(PropertyRepository $repository, ObjectManager $em)
+    {
+        $this->repository = $repository;
+        $this->em = $em;
+    }
+
+    /**
+     * @Route("/offer", name="property.index")
+     * @return Response
+     */
+    public function index(): Response
+    {
+        return $this->render('property/index.html.twig', ['current_menu' => 'properties']);
+    }
+}
